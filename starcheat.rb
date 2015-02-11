03276aac573a54fcfe60d9502676924d3c316a29
Christian Moritz <chrmoritz@users.noreply.github.com>
2015-02-11 21:40:00 +0100

    updated to 0.19.1

--- starcheat.rb
+++ starcheat.rb
@@ -1,6 +1,6 @@
 class Starcheat < Formula
   homepage "https://github.com/wizzomafizzo/starcheat"
-  url "https://github.com/wizzomafizzo/starcheat.git", :tag => "0.19"
+  url "https://github.com/wizzomafizzo/starcheat.git", :tag => "0.19.1"
 
   head "https://github.com/wizzomafizzo/starcheat.git", :branch => "dev"
   depends_on :python3
