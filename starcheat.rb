d771251263d48f21d435108c37d25c7d3ff41784
Christian Moritz <chrmoritz@users.noreply.github.com>
2015-04-22 19:49:48 +0200

    added revision to tag (fixed audit warning)

--- starcheat.rb
+++ starcheat.rb
@@ -1,6 +1,7 @@
 class Starcheat < Formula
   homepage "https://github.com/wizzomafizzo/starcheat"
-  url "https://github.com/wizzomafizzo/starcheat.git", :tag => "0.22"
+  url "https://github.com/wizzomafizzo/starcheat.git", :tag => "0.22",
+    :revision => "3ef590fdaa7a9d73955c57f183da768b3319376b"
 
   head "https://github.com/wizzomafizzo/starcheat.git", :branch => "dev"
   depends_on :python3
