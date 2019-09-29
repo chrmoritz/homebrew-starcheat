1d8d776cf851b97e11832bd29799c84cc725b2f6
Christian Moritz <chrmoritz@users.noreply.github.com>
2019-09-30 00:11:35 +0200

    Update starcheat.rb

--- starcheat.rb
+++ starcheat.rb
@@ -3,7 +3,7 @@ class Starcheat < Formula
   desc "Starbound player save file editor"
   homepage "https://github.com/wizzomafizzo/starcheat"
   url "https://github.com/wizzomafizzo/starcheat.git",
-      :tag => "0.27.1",
+      :tag      => "0.27.1",
       :revision => "17dd471bc223d5c8a1a74f1e4a2eee035246ce36"
   head "https://github.com/wizzomafizzo/starcheat.git", :branch => "dev"
 
@@ -12,18 +12,12 @@ class Starcheat < Formula
     version "1.0"
   end
 
-  depends_on "python"
-  depends_on "pyqt"
+  depends_on "freetype"
   depends_on "jpeg"
-
-  resource "Pillow" do
-    url "https://files.pythonhosted.org/packages/0f/57/25be1a4c2d487942c3ed360f6eee7f41c5b9196a09ca71c54d1a33c968d9/Pillow-5.0.0.tar.gz"
-    sha256 "12f29d6c23424f704c66b5b68c02fe0b571504459605cfe36ab8158359b0e1bb"
-  end
+  depends_on "pyqt"
+  depends_on "python"
 
   def install
-    venv = virtualenv_create(libexec)
-    venv.pip_install resources
     system "python3", "build.py", "-v"
     libexec.install Dir["build/*"]
     bin.install_symlink libexec/"starcheat.py" => "starcheat"
@@ -74,6 +68,9 @@ class Starcheat < Formula
 
   def caveats
     <<~EOS
+      NOTICE: Please install the required Pillow (image library) depednecy yourself:
+        `pip3 install --upgrade Pillow`
+
       You can run this to symlink the StarCheat.app into your Application folder:
         `brew linkapps starcheat`
       or just run if from:
